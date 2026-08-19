3f86b3dd972172852a9c74735df641684ac3e11b
solyxassistencia-a11y <solyxassistencia@gmail.com>
2026-08-19 17:06:54 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,64 +1,47 @@
-const CACHE_NAME = "solyx-fidelidade-v14";
+const CACHE_NAME = "AdminFidelidade-v1";
+const OFFLINE_URL = "./offline.html";
 
-const FILES_TO_CACHE = [
+const urlsToCache = [
     "./",
-    "./index.html",
-    "./manifest.json"
+    "./offline.html"
 ];
 
-
-// INSTALAÇÃO
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(cache => cache.addAll(urlsToCache))
             .then(() => self.skipWaiting())
-
     );
-
 });
 
-
-// ATIVAÇÃO
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
         caches.keys().then(cacheNames => {
-
             return Promise.all(
-
                 cacheNames
                     .filter(cacheName => cacheName !== CACHE_NAME)
                     .map(cacheName => caches.delete(cacheName))
-
             );
-
         }).then(() => self.clients.claim())
-
     );
-
 });
 
-
-// REQUISIÇÕES
 self.addEventListener("fetch", event => {
 
+    if (event.request.mode === "navigate") {
+
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => caches.match(OFFLINE_URL))
+        );
+
+        return;
+    }
+
     event.respondWith(
-
         caches.match(event.request)
-            .then(cachedResponse => {
-
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return fetch(event.request);
-
+            .then(response => {
+                return response || fetch(event.request);
             })
-
     );
-
 });
