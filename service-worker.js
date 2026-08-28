d9c8e8b66ad9ec53577fa6fb54f91436e1d4be20
solyxassistencia-a11y <solyxassistencia@gmail.com>
2026-08-27 23:13:20 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -31,7 +31,7 @@
    1. CONFIGURAÇÃO
    ============================================================ */
 
-const CACHE_NAME = "SolyxLojista-v44";
+const CACHE_NAME = "SolyxLojista-v45";
 
 const OFFLINE_PAGE = "./offline.html";
 
